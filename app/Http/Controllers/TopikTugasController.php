5a0e16f93d0d74254e6bc750786f87fc5a7a6bfe
jipyy <jahdanpandita16@gmail.com>
2024-08-30 10:42:32 +0700

    membuat table baru dan mcv : tugas, nilai_tugas, topik_tugas, komentar_tugas

---
+++ app/Http/Controllers/TopikTugasController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TopikTugas;
+use Illuminate\Http\Request;
+
+class TopikTugasController extends Controller
+{
+    public function index()
+    {
+        // $topikTugas = TopikTugas::all();
+        // return view('topik_tugas.index', compact('topikTugas'));
+    }
+
+    public function create()
+    {
+        // return view('topik_tugas.create');
+    }
+
+    public function store(Request $request)
+    {
+        // $request->validate([
+        //     'judul_topik' => 'required|string|max:255',
+        // ]);
+
+        // TopikTugas::create($request->all());
+
+        // return redirect()->route('topik_tugas.index')
+        //     ->with('success', 'Topik tugas berhasil ditambahkan.');
+    }
+
+    public function show(TopikTugas $topikTugas)
+    {
+        // return view('topik_tugas.show', compact('topikTugas'));
+    }
+
+    public function edit(TopikTugas $topikTugas)
+    {
+        // return view('topik_tugas.edit', compact('topikTugas'));
+    }
+
+    public function update(Request $request, TopikTugas $topikTugas)
+    {
+        // $request->validate([
+        //     'judul_topik' => 'required|string|max:255',
+        // ]);
+
+        // $topikTugas->update($request->all());
+
+        // return redirect()->route('topik_tugas.index')
+        //     ->with('success', 'Topik tugas berhasil diperbarui.');
+    }
+
+    public function destroy(TopikTugas $topikTugas)
+    {
+        // $topikTugas->delete();
+
+        // return redirect()->route('topik_tugas.index')
+        //     ->with('success', 'Topik tugas berhasil dihapus.');
+    }
+}
+
