0d5d0b3dd573cc620e5b2235b30d0f53626638f8
ryunex15 <badarudin1511@gmail.com>
2024-09-06 09:28:42 +0700

    menghapus nav item belum diserahkan

--- resources/js/src/views/pages/profile/ProfileTugas.vue
+++ resources/js/src/views/pages/profile/ProfileTugas.vue
@@ -1,67 +1,64 @@
 <template>
-<b-card no-body class="main-card">
-    <b-overlay :show="isBusy" rounded opacity="0.6" size="lg" spinner-variant="primary">
-        <b-card-body class="p-0">
-            <ul class="nav nav-pills nav-fill mb-3 nav-pills-custom">
-                <li class="nav-item">
-                    <a class="nav-link text-sm text-md text-lg" :class="{ active: activeTab === 'ditugaskan' }" href="#" @click.prevent="setTab('ditugaskan')">
-                        Ditugaskan
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-sm text-md text-lg" :class="{ active: activeTab === 'belumDiserahkan' }" href="#" @click.prevent="setTab('belumDiserahkan')">
-                        Belum Diserahkan
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-sm text-md text-lg" :class="{ active: activeTab === 'selesai' }" href="#" @click.prevent="setTab('selesai')">
-                        Selesai
-                    </a>
-                </li>
-            </ul>
+    <b-card no-body class="main-card">
+        <b-overlay
+            :show="isBusy"
+            rounded
+            opacity="0.6"
+            size="lg"
+            spinner-variant="primary"
+        >
+            <b-card-body class="p-0">
+                <ul class="nav nav-pills nav-fill mb-3 nav-pills-custom">
+                    <li class="nav-item">
+                        <a
+                            class="nav-link text-sm text-md text-lg"
+                            :class="{ active: activeTab === 'ditugaskan' }"
+                            href="#"
+                            @click.prevent="setTab('ditugaskan')"
+                        >
+                            Ditugaskan
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a
+                            class="nav-link text-sm text-md text-lg"
+                            :class="{ active: activeTab === 'selesai' }"
+                            href="#"
+                            @click.prevent="setTab('selesai')"
+                        >
+                            Selesai
+                        </a>
+                    </li>
+                </ul>
 
-            <div class="header-banner text-white p-4 mb-4 text-md-start">
-                <h1 class="display-6 display-md-4 mb-1 text-white">
-                    Kelas X RPL 2
-                </h1>
-                <p class="lead mb-0">Pemrograman web</p>
-                <p class="mb-0">Dudung Dzulkifli</p>
-            </div>
+                <div class="header-banner text-white p-4 mb-4 text-md-start">
+                    <h1 class="display-6 display-md-4 mb-1 text-white">
+                        Kelas X RPL 2
+                    </h1>
+                    <p class="lead mb-0">Pemrograman web</p>
+                    <p class="mb-0">Dudung Dzulkifli</p>
+                </div>
 
-            <div class="container">
-                <b-row>
-                    <b-col md="12">
-                        <!-- Ditugaskan -->
-                        <b-card class="mb-4 post-card shadow-sm rounded border-0" v-if="activeTab === 'ditugaskan'" v-for="(item, index) in topik" :key="index">
-                            <b-card-body>
-                                <p class="responsive-text">
-                                    <b>{{ item.judul_topik }}</b> - Mempelajari Akupuntur.
-                                </p>
-                                <b-button variant="info" class="ml-auto" @click="showTaskDetails">Detail Tugas</b-button>
-                            </b-card-body>
-                        </b-card>
-
-                            <!-- Belum Diserahkan -->
+                <div class="container">
+                    <b-row>
+                        <b-col md="12">
+                            <!-- Ditugaskan -->
                             <b-card
-                                class="mb-4 post-card shadow-sm rounded border border-warning"
-                                v-if="activeTab === 'belumDiserahkan'"
+                                class="mb-4 post-card shadow-sm rounded border-0"
+                                v-if="activeTab === 'ditugaskan'"
+                                v-for="(item, index) in topik"
+                                :key="index"
                             >
-                                <b-card-body
-                                    class="d-flex justify-content-between"
-                                >
+                                <b-card-body>
                                     <p class="responsive-text">
-                                        <b>Tugas 1</b> - Mempelajari Linkedin.
-                                        <br />
-                                        <b
-                                            >Batas Penyerahan : Selasa 3
-                                            September 2024</b
-                                        >
+                                        <b>{{ item.judul_topik }}</b> -
+                                        Mempelajari Akupuntur.
                                     </p>
                                     <b-button
-                                        variant="success"
+                                        variant="info"
                                         class="ml-auto"
-                                        @click="showSubmitTask"
-                                        >Kirim Tugas</b-button
+                                        @click="showTaskDetails"
+                                        >Detail Tugas</b-button
                                     >
                                 </b-card-body>
                             </b-card>
@@ -112,36 +109,58 @@
                 class="mb-4 w-25 d-block mx-auto"
             />
 
-        <h5>
-            Manfaat akupuntur juga bisa membantu meredakan nyeri sendi.
-            Akupuntur bisa dipakai untuk terapi penyembuhan cedera pada
-            lutut. Penderitanya perlu melakukan akupuntur secara berulang
-            untuk menurunkan gejala nyerinya.
-        </h5>
-        <br />
-        <b-button variant="secondary" @click="isDetailModalVisible = false">Tutup</b-button>
-    </b-modal>
+            <h5>
+                Manfaat akupuntur juga bisa membantu meredakan nyeri sendi.
+                Akupuntur bisa dipakai untuk terapi penyembuhan cedera pada
+                lutut. Penderitanya perlu melakukan akupuntur secara berulang
+                untuk menurunkan gejala nyerinya.
+            </h5>
+            <br />
+            <b-button variant="secondary" @click="isDetailModalVisible = false"
+                >Tutup</b-button
+            >
+        </b-modal>
 
-    <!-- Modal for Kirim Tugas -->
-    <b-modal id="submit-modal" title="Kirim Tugas" v-model="isSubmitModalVisible" hide-footer centered>
-        <b-form @submit.prevent="submitTask">
-            <b-form-group label="Nama" label-for="nama-input">
-                <b-form-input id="nama-input" v-model="nama" required></b-form-input>
-            </b-form-group>
+        <!-- Modal for Kirim Tugas -->
+        <b-modal
+            id="submit-modal"
+            title="Kirim Tugas"
+            v-model="isSubmitModalVisible"
+            hide-footer
+            centered
+        >
+            <b-form @submit.prevent="submitTask">
+                <b-form-group label="Nama" label-for="nama-input">
+                    <b-form-input
+                        id="nama-input"
+                        v-model="nama"
+                        required
+                    ></b-form-input>
+                </b-form-group>
 
-            <b-form-group label="Kelas" label-for="kelas-input">
-                <b-form-input id="kelas-input" v-model="kelas" required></b-form-input>
-            </b-form-group>
+                <b-form-group label="Kelas" label-for="kelas-input">
+                    <b-form-input
+                        id="kelas-input"
+                        v-model="kelas"
+                        required
+                    ></b-form-input>
+                </b-form-group>
 
-            <b-form-group label="Lampirkan File" label-for="file-input">
-                <b-form-file id="file-input" v-model="file" required></b-form-file>
-            </b-form-group>
+                <b-form-group label="Lampirkan File" label-for="file-input">
+                    <b-form-file
+                        id="file-input"
+                        v-model="file"
+                        required
+                    ></b-form-file>
+                </b-form-group>
 
-            <b-button type="submit" variant="success">Kirim</b-button>
-            <b-button variant="danger" @click="isSubmitModalVisible = false">Batal</b-button>
-        </b-form>
-    </b-modal>
-</b-card>
+                <b-button type="submit" variant="success">Kirim</b-button>
+                <b-button variant="danger" @click="isSubmitModalVisible = false"
+                    >Batal</b-button
+                >
+            </b-form>
+        </b-modal>
+    </b-card>
 </template>
 
 <script>
@@ -189,11 +208,10 @@ export default {
             nama: "",
             kelas: "",
             file: null,
-            items: []
-        }
+            items: [],
+        };
     },
     methods: {
-
         setTab(tab) {
             this.activeTab = tab;
         },
